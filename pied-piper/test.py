40d2497e059b3d68390be772095684bb66d69fd0
Aslan Noorghasemi <42834665+aslan-ng@users.noreply.github.com>
2022-10-17 09:13:24 +0330

    targetted tests are now possible

--- pied-piper/test.py
+++ pied-piper/test.py
@@ -1,9 +1,21 @@
 import unittest
 
 
-loader = unittest.TestLoader()
-start_dir = 'test'
-suite = loader.discover(start_dir)
+def test(target:str='all'):
+    suite = None
+    loader = unittest.TestLoader()
+    start_dir = 'test'
+    if target == 'all':
+        suite = loader.discover(start_dir)
+    else:
+        pattern = start_dir + '_' + target + '.py'
+        suite = loader.discover(start_dir, pattern=pattern)
+    return suite
+    
 
-runner = unittest.TextTestRunner()
-runner.run(suite)
+if __name__ == "__main__":
+    target = 'settlement'
+    #target = 'all'
+    suite = test(target=target)
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
