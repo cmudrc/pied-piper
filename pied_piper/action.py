fc3c3f05de0ca778ceff13af39b1a5ae77be4934
Aslan Noorghasemi <42834665+aslan-ng@users.noreply.github.com>
2022-10-19 16:38:40 +0330

    action class done

--- pied_piper/action.py
+++ pied_piper/action.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from tools import dt
 from tools import find_element
 
 
@@ -20,22 +21,58 @@ class Action:
         self.transportation = transportation
 
     def update(self, all_settlements):
+        """
+        Read the desired info from other elements
+        """
         self.start_pos = find_element(self.start_point, all_settlements).pos
         self.end_pos = find_element(self.end_point, all_settlements).pos
 
     def travel_duration(self):
-        pass
+        """
+        Total duration of the action
+        """
+        vector = self.total_displacement_vector()
+        vector_size = np.sqrt(np.sum(vector**2))
+        delta_t_SI = vector_size / self.transportation.speed
+        return dt(seconds=delta_t_SI)
 
     def when_reach(self):
-        pass
-    
+        """
+        Calculate the date in which the destination will be reached
+        """
+        delta_t = self.travel_duration()
+        return self.start_date + delta_t
+
     def pos(self, date):
-        delta_t = (self.start_date - date).seconds
-        print(delta_t)
-        displacement_vector = self.direction_vector() * delta_t
-        return self.start_pos + displacement_vector
+        """
+        Calculate the position in the requested date
+        """
+        result = None
+        action_progress = self.action_progress(date)
+        if action_progress < 0:
+            result = self.start_pos
+        elif action_progress >= 0 and action_progress <= 1:
+            current_displacement_vector = action_progress * self.total_displacement_vector()
+            result = self.start_pos + current_displacement_vector
+        elif action_progress > 1:
+            result = self.end_pos
+        return result
+
+    def action_progress(self, date):
+        result = None
+        end_date = self.when_reach()
+        if date < self.start_date:
+            result = 0
+        elif date >= self.start_date and date <= end_date:
+            result = (date - self.start_date).seconds / (end_date - self.start_date).seconds
+        else:
+            result = 1
+        return result
 
     def total_displacement_vector(self):
+        """
+        Total displacement vector between starting point and ending point
+        """
         return np.array(self.end_pos) - np.array(self.start_pos)
 
     def direction_vector(self):
@@ -63,7 +100,7 @@ if __name__ == "__main__":
 
     s_2 = Settlement(
         name='settlement_2',
-        pos=[1, 0.2],
+        pos=[1000, 0],
         max_population=10,
         boundery={
             'type': 'circular',
@@ -83,5 +120,6 @@ if __name__ == "__main__":
     a.update(settlements)
     #print(a.total_displacement_vector())
     #print(a.direction_vector())
-    print(start_date + dt(seconds=1000))
-    print(a.pos(start_date + dt(seconds=1000)))
+    print(start_date + dt(seconds=360))
+    print(a.pos(start_date + dt(seconds=360)))
+    #print(a.when_reach())
--- pied_piper/test/test_action.py
+++ pied_piper/test/test_action.py
@@ -1,7 +1,8 @@
 import unittest
+import numpy as np
 
 from action import Action
-from tools import date
+from tools import date, dt
 from transportation import Foot
 from settlement import Settlement
 
@@ -19,7 +20,7 @@ class TestActionClass(unittest.TestCase):
 
     s_2 = Settlement(
         name='settlement_2',
-        pos=[1, 1],
+        pos=[1000, 0],
         max_population=10,
         boundery={
             'type': 'circular',
@@ -37,10 +38,38 @@ class TestActionClass(unittest.TestCase):
     def test_update(self):
         settlements = [self.s_1, self.s_2]
         self.a.update(settlements)
-        self.assertListEqual(self.a.end_pos, [1, 1])
-        self.assertListEqual(self.a.start_pos, [0, 0])
+        self.assertListEqual(self.a.start_pos, self.s_1.pos)
+        self.assertListEqual(self.a.end_pos, self.s_2.pos)
 
-    def test_(self):
+    def test_total_displacement_vector(self):
         settlements = [self.s_1, self.s_2]
         self.a.update(settlements)
-        self.assertListEqual(self.a.end_pos, [1, 1])
+        self.assertListEqual(list(self.a.total_displacement_vector()), self.s_2.pos)
+
+    def test_direction_vector(self):
+        settlements = [self.s_1, self.s_2]
+        self.a.update(settlements)
+        self.assertListEqual(list(self.a.direction_vector()), [1, 0])
+
+    def test_travel_duration(self):
+        settlements = [self.s_1, self.s_2]
+        self.a.update(settlements)
+        self.assertEqual(self.a.travel_duration().seconds, 720)
+
+    def test_pos_0(self):
+        settlements = [self.s_1, self.s_2]
+        self.a.update(settlements)
+        date = self.a.start_date - dt(hours=1)
+        self.assertListEqual(list(self.a.pos(date)), self.s_1.pos)
+
+    def test_pos_1(self):
+        settlements = [self.s_1, self.s_2]
+        self.a.update(settlements)
+        date = self.a.start_date + dt(seconds=360)
+        self.assertListEqual(list(self.a.pos(date)), [500, 0])
+
+    def test_pos_2(self):
+        settlements = [self.s_1, self.s_2]
+        self.a.update(settlements)
+        date = self.a.start_date + dt(hours=1)
+        self.assertListEqual(list(self.a.pos(date)), self.s_2.pos)
