fcf3404f2e70c31cf4f02b58d15a186c786a0bb5
Aslan Noorghasemi <42834665+aslan-ng@users.noreply.github.com>
2022-10-29 12:55:27 +0330

    egg-info folder added to gitignore

--- pied_piper/test.py
+++ pied_piper/test.py
@@ -14,8 +14,8 @@ def test(target:str='all'):
     
 
 if __name__ == "__main__":
-    target = 'move'
-    #target = 'all'
+    #target = 'move'
+    target = 'all'
     suite = test(target)
     runner = unittest.TextTestRunner()
     runner.run(suite)
