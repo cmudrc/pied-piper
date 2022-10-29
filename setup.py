948dd45ebd26769c2513d446d2b2c987f6833f31
Aslan Noorghasemi <42834665+aslan-ng@users.noreply.github.com>
2022-10-29 15:58:25 +0330

    setup.py, gitignore and BASEMENT are updated

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
 from setuptools import setup, find_packages
 
-setup(name='pied_piper', version='1.0', packages=find_packages())
+setup(
+    name='pied_piper',
+    version='1.0',
+    description='An Agent-Based Model Framework',
+    author='',
+    author_email='',
+    url='',
+    packages=find_packages()
+)
