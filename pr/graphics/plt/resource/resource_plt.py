33562acab288912348eb4b1520494954a1a1d811
Aslan Noorghasemi <42834665+aslan-ng@users.noreply.github.com>
2022-11-07 11:13:27 +0330

    resource graphical representation

--- pr/graphics/plt/resource/resource_plt.py
+++ pr/graphics/plt/resource/resource_plt.py
@@ -2,11 +2,9 @@ import matplotlib.pyplot as plt
 
 
 style = {
-    'fill': False,
-    'linestyle': '--',
-    'linewidth': None,
-    'color': 'blue',
-    'alpha': 0.5,
+    'width': 0.3,
+    'color_good': 'b',
+    'color_bad': 'r',
 }
 
 def use_to_plt(dictionary: dict, ax=None):
@@ -23,13 +21,47 @@ def storage_to_plt(dictionary: dict, ax=None):
     if ax is None:
         ax = plt.gca()
     d = dictionary
+    footer = ['storage']
+    ax.bar(
+        footer,
+        d['current_amount'],
+        style['width'],
+        color=style['color_good']
+    )
+    ax.bar(
+        footer,
+        d['max_amount'],
+        style['width'],
+        color=style['color_bad']
+    )
 
 def deficiency_to_plt(dictionary: dict, ax=None):
     if ax is None:
         ax = plt.gca()
     d = dictionary
+    footer = ['deficiency']
+    ax.bar(
+        footer,
+        d['current_amount'],
+        style['width'],
+        color=style['color_bad']
+    )
+    ax.bar(
+        footer,
+        d['max_amount'],
+        style['width'],
+        color=style['color_good']
+    )
 
 def resource_to_plt(dictionary: dict, ax=None):
     if ax is None:
         ax = plt.gca()
     d = dictionary
+    if d['use'] is not None:
+        pass
+    if d['produce'] is not None:
+        pass
+    if d['storage'] is not None:
+        pass
+    if d['deficiency'] is not None:
+        pass
