7e7cee52be5b83ada097229316cc4e40e2b18b7a
aslan-ng <anoorghasemi@gmail.com>
2023-02-11 13:05:03 +0330

    log enhancement

--- piperabm/log/log.py
+++ piperabm/log/log.py
@@ -5,7 +5,7 @@ class Log:
     def __init__(self, reset=False):
         self.file_name = "log.txt"
         if reset is True:
-            f.open(self.file_name, "w")
+            f = open(self.file_name, "w")
         else:
             f = open(self.file_name, "a")
         f.close()
@@ -14,3 +14,23 @@ class Log:
         f = open(self.file_name, "a")
         f.write(txt+'\n')
         f.close()
+
+'''
+class Log2:
+    file_name = "log.txt"
+    first_call = True
+    
+    def __init__(self):
+        if self.first_call is False:
+            file = open(self.file_name, "a")
+        elif self.first_call is True:
+            # file exists -> reset
+            # or else create it
+            f = open(self.file_name, "w")
+            self.first_call = False
+
+    def add(self, txt):
+        f = open(self.file_name, "a")
+        f.write(txt+'\n')
+        f.close()
+'''
