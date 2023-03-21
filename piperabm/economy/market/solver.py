be5da407118c599f928f444a68f284c268ecc7cb
aslan-ng <anoorghasemi@gmail.com>
2023-03-21 23:23:43 +0330

    market solver is getting required enhancements

--- piperabm/economy/market/solver.py
+++ piperabm/economy/market/solver.py
@@ -2,6 +2,70 @@ try: from .pool import Pool, Bid
 except: from pool import Pool, Bid
 
 
+class NewSolver:
+
+    def solve(self):
+        while True:
+            self.solve
+
+    def create_pools(self):
+
+        def create_bid(player, resource_name):
+            t_s = self.total_source(resource_name)
+            #t_d = self.total_demand(resource)
+            t_d_a = self.total_actual_demand(resource_name)
+            player_source = player.new_source[resource_name]
+            player_actual_demand = player.actual_demand(self.exchange)
+            player_demand = player_actual_demand[resource_name]
+            others_source = t_s - player_source
+            others_demand = t_d_a - player_demand
+            seller_score = others_demand * player_source
+            buyer_score = others_source * player_demand
+            if seller_score >= buyer_score:
+                bid = Bid(agent=player.index, amount=player_source)
+                type = 'source'
+            else:
+                bid = Bid(agent=player.index, amount=player_demand)
+                type = 'demand'
+            return bid, type
+        
+        pools = {}
+        for resource_name in self.all_resources():
+            pool = Pool()
+            for player in self.players:
+                bid, type = create_bid(player, resource_name)
+                if type == 'source':
+                    pool.add_source(bid)
+                elif type == 'demand':
+                    pool.add_demand(bid)
+            pools[resource_name] = pool
+        return pools
+    
+    def sort_pools(self, pools):
+        pools_score = {}
+        result = None
+        for resource_name in pools:
+            pool = pools[resource_name]
+            size_source, size_demand = pool.size()
+            pools_score[resource_name] = size_source + size_demand
+            sorted_pools = sorted(pools_score.items(), key=lambda x:x[1], reverse=True)
+            sorted_pools = list(list(zip(*sorted_pools))[0])
+            result = sorted_pools
+        #print(pools_score)
+        return result
+    
+    def solve_biggest(self, pools):
+        pools = self.create_pools()
+        #print(len(pools))
+        sorted_pools = self.sort_pools(pools)
+        if sorted_pools is not None:
+            resource = sorted_pools[0] # biggest pool
+            self.solve_single(pools, resource) ######
+        return pools
+
+
+
+
 class Solver:
 
     def solve(self):
