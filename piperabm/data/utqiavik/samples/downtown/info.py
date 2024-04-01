00574a86262d39eecb7bc0e56695914c0de57b70
aslan-ng <anoorghasemi@gmail.com>
2024-04-01 16:04:51 -0400

    downtown info refined

--- piperabm/data/utqiavik/samples/downtown/info.py
+++ piperabm/data/utqiavik/samples/downtown/info.py
@@ -2,7 +2,7 @@ name = 'utqiavik_downtown'
 
 # Boundary
 point_1_latitude, point_1_longitude = 71.293255, -156.776608
-point_2_latitude, point_2_longitude = 71.308299, -156.713400
+point_2_latitude, point_2_longitude = 71.308299, -156.708000
 
 latitude_min = min([point_1_latitude, point_2_latitude])
 latitude_max = max([point_1_latitude, point_2_latitude])
