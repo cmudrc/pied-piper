f3b158c12c06310b6663ecc955dcaa6226127543
aslan-ng <anoorghasemi@gmail.com>
2024-06-19 14:09:42 -0400

    degradation is added as a separate method

--- piperabm/infrastructure/degradation.py
+++ piperabm/infrastructure/degradation.py
@@ -17,6 +17,17 @@ class Degradation:
                     weather_impact=weather_impact
                 )
     
+    def degradation(self, ids: list) -> float:
+        """
+        Calculate current degradation for an edge
+        """
+        usage_impact = self.get_edge_attribute(ids=ids, attribute='usage_impact')
+        weather_impact = self.get_edge_attribute(ids=ids, attribute='weather_impact')
+        return self.adjustment_factor(
+            usage_impact=usage_impact,
+            weather_impact=weather_impact
+        )
+    
     def top_degraded_edges(self, percent: float = 0):
         """
         Filter most degradaded edges by their length percentage
@@ -26,13 +37,13 @@ class Degradation:
         edges_info = []
         for edge_ids in edges_ids:
             length = self.get_edge_attribute(ids=edge_ids, attribute='length')
-            total_length += length
             edge_info = {
                 'ids': edge_ids,
-                'degradation': self.get_edge_attribute(ids=edge_ids, attribute='degradation'),
+                'degradation': self.degradation(id=edge_ids),
                 'length': length
             }
             edges_info.append(edge_info)
+            total_length += length
         sorted_edges_info = sorted(edges_info, key=lambda x: x['degradation'], reverse=True)
         remaining_length = (percent / 100) * total_length
         result = []
