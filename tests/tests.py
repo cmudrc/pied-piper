5360e240a3c909c6894425fe69531293862bdc68
Aslan Noorghasemi <anoorghasemi@gmail.com>
2025-05-18 17:52:47 -0400

    automatic tests fixed

--- tests/tests.py
+++ tests/tests.py
@@ -1,29 +1,12 @@
 import unittest
 
 
-class Test:
-    """
-    Run tests in batch
-    """
-    def __init__(self, directory='test'):
-        self.directory = directory
-        self.loader = unittest.TestLoader()
-        self.runner = unittest.TextTestRunner()
+def run():
+    loader = unittest.TestLoader()
+    suite = loader.discover(start_dir="tests", pattern="test_*.py")
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
 
-    def discover(self, target: str):
-        suite = None
-        if target == 'all':
-            suite = self.loader.discover(self.directory)
-        else:
-            pattern = self.directory + '_' + target + '.py'
-            suite = self.loader.discover(self.directory, pattern)
-        return suite
-    
-    def run(self, target: str = 'all'):
-        suite = self.discover(target)
-        self.runner.run(suite)
-    
 
 if __name__ == "__main__":
-    test = Test()
-    test.run('all')
+    run()
