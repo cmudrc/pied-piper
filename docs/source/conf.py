fdd32c8225ee5cd94cb1c8e11a3f9d5476d67b92
Aslan Noorghasemi <anoorghasemi@gmail.com>
2026-01-13 14:26:37 -0500

    Release v0.2.0: explicit behavior registration and docs updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 project = "PiperABM"
 copyright = "2025, Aslan Noorghasemi, Christopher McComb"
 author = "Aslan Noorghasemi, Christopher McComb"
-release = "0.1.2"
+release = "0.2.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
